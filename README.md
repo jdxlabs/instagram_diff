2baa9fc4cf9084da2a72fec5aa54a10168de3a86
jdxlabs <jdevoucoux@gmail.com>
2018-05-30 09:01:08 +0200

    it works ^^

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # instagram_diff
+
+## Description
+This script will tell you the users specific to each one of your lists on Instagram : followers & following.
+
+## Requirements
+* Python 2.6+
+* [Instagram module for Python](https://github.com/LevPasha/Instagram-API-python) (pip install InstagramApi)
+
+## Execution
+First, create a conf.json file with you application keys and the tokens of the account you want to treat.
+Then, just launch the script diff.py, it will return something like that :
+```
+
+5623 followers (users who follow the account).
+8422 followings (users subscribed by the account).
+
+528 followersOnly : [ <usernames list> ]
+
+256 followingsOnly : [ <usernames list> ]
+```
---
+++ diff.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import json
+import time
+from datetime import datetime
+from InstagramAPI import InstagramAPI
+with open('./conf.json') as file:
+    conf = json.load(file)
+
+def getFollowers(api, user_id):
+    followers = []
+    next_id = True
+    while next_id:
+        if next_id is True:
+            next_id = ''
+        api.getUserFollowers(user_id, maxid=next_id)
+        res = api.LastJson
+        for user in res.get('users', []):
+            followers.append(user['username'])
+        next_id = res.get('next_max_id', '')
+    return followers
+
+def getFollowings(api, user_id):
+    followings = []
+    next_id = True
+    while next_id:
+        if next_id is True:
+            next_id = ''
+        api.getUserFollowings(user_id, maxid=next_id)
+        res = api.LastJson
+        for user in res.get('users', []):
+            followings.append(user['username'])
+        next_id = res.get('next_max_id', '')
+    return followings
+
+api = InstagramAPI(conf['username'], conf['password'])
+api.login()
+user_id = api.username_id
+
+followers = getFollowers(api, user_id)
+followings = getFollowings(api, user_id)
+
+print("%s followers (users who follow the account)." % len(followers))
+print("%s followings (users subscribed by the account)." % len(followings))
+print("\n")
+
+followersOnly = list(set(followers) - set(followings))
+followingsOnly = list(set(followings) - set(followers))
+print("%i followersOnly : %s\n" % (len(followersOnly), json.dumps(followersOnly)))
+print("%i followingsOnly : %s\n" % (len(followingsOnly), json.dumps(followingsOnly)))
